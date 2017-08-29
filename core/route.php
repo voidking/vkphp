9015bb28f2b66181993259c5674bd49aaa88c853
voidking <voidking@qq.com>
2017-08-29 22:53:03 +0800

    获取url中的参数

--- core/route.php
+++ core/route.php
@@ -7,19 +7,20 @@ namespace core;
 class route{
     public $ctrl;
     public $action;
-    public $params;
+    public $params=array();
     public function __construct(){
         //echo 'route is ready!';
 
         /**
          * 1、隐藏index.php
          * 2、获取URL中的控制器和方法
+         * 3、获取URL中的参数
          */
 
         if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] != '/'){
             $path = $_SERVER['REQUEST_URI'];
             $patharr = explode('/',trim($path, '/'));
-            p($patharr);
+            //p($patharr);
 
             if(isset($patharr[0])){
                 if($patharr[0] != 'index.php'){
@@ -31,7 +32,12 @@ class route{
                     } else{
                         $this->action = 'index';
                     }
-
+                    $count = count($patharr);
+                    $i=2;
+                    while($i < $count){
+                        $this->params[$patharr[$i]] = $patharr[$i+1];
+                        $i = $i + 2;
+                    }
                 }else{
                     // 没省略index.php
                     if(isset($patharr[1])){
@@ -42,15 +48,23 @@ class route{
                     } else{
                         $this->action = 'index';
                     }
+
+                    $count = count($patharr);
+                    $i=3;
+                    while($i < $count){
+                        $this->params[$patharr[$i]] = $patharr[$i+1];
+                        $i = $i + 2;
+                    }
                 }
             }else{
                 $this->ctrl = 'index';
                 $this->action = 'index';
             }
 
-        }else{
+        }else {
             $this->ctrl = 'index';
             $this->action = 'index';
         }
+        p($this->params);
     }
 }
