b2bf49ed76f4fbc2c9738f23c00a94e0b0ae5faf
voidking <voidking@qq.com>
2017-09-13 08:16:50 +0800

    修改日志类

--- core/log.php
+++ core/log.php
@@ -6,7 +6,7 @@ class log{
 
     public function log($message,$file_name){
         $log_path = LOG.'/'.$file_name.'-'.date('YmdHis').'.log';
-        $message = $message.' '.date('Y-m-d H:i:s');
+        $message = date('Y-m-d H:i:s').' '.$message;
         file_put_contents($log_path,json_encode($message));
     }
 }
