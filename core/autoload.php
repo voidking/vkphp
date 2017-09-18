d703052030423c825eebbd30dc83b7f7ab108b12
voidking <voidking@qq.com>
2017-09-18 16:37:25 +0800

    修改autoload，使之支持linux系统

--- core/autoload.php
+++ core/autoload.php
@@ -6,6 +6,10 @@ namespace core;
 
 class autoload{
     public static function load($class_name){
+        $os = strtoupper(substr(PHP_OS,0,3))==='WIN'? 'windows':'linux';
+        if($os == 'linux'){
+            $class_name = str_replace('\\','/',$class_name);
+        }
         if(file_exists($class_name.'.php')){
             require_once $class_name.'.php';
             return true;
